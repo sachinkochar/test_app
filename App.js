259d0224b2ad083b18f0d03455473b676f087773
expert <expert.developer.it@gmail.com>
2018-06-20 18:12:26 +0530

    initial setup done

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './images/tpg-logo.png';
+import './css/App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Post Comments</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
